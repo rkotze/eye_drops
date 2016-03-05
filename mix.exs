fdb5dd5cfd2209f9788b663ab3902dafecb87061
Richard Kotze <rkotze@dctfh.com>
2016-03-05 16:48:23 +0000

    elixir compatible version fixed.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mix.Tasks.EyeDrops.Mixfile do
   def project do
     [app: :eye_drops,
      version: "1.1.1",
-     elixir: ">= 1.1 and <= 1.2.3",
+     elixir: ">= 1.1.0 and <= 1.2.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
